152fea5fbb510a35eea383d0e99ceaa404bfa6aa
akshay <akshaybabar2408@gmail.com>
2025-05-01 16:54:26 +0530

    Refactor calculate_sum to support any number of comma-separated inputs

--- app/lib/calculator_string.rb
+++ app/lib/calculator_string.rb
@@ -1,8 +1,6 @@
 class CalculatorString
   def calculate_sum(string_numbers)
     return 0 if string_numbers.empty?
-    return string_numbers.to_i if string_numbers.match(/^\d+$/)
-
     string_numbers.split(',').sum(&:to_i)
   end
 end
